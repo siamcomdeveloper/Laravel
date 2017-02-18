8350cbac78eaeb71148a297331c6471ecebf2f84
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 06:44:08 +0700

    form

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -123,14 +123,16 @@
             </div>
         </div>
         <div id="style-well2" class="well">
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img class="topic-img" src="http://lorempixel.com/320/240?random3">
-  		</a>
-  		<div class="media-body">
-                    <h4 class="media-heading">When will the concert be held?</h4>
-                    <p class="post-body">After day, don't sixth were divide creepeth living him it heaven moveth of male man rule, made gathered cattle after. For gathering place seed bearing called and let shall fly fruitful fruitful creeping them brought years beginning air to beginning, good don't one abundantly give. That were blessed made moving light saw place they're. Moved his life moved open for midst sea called grass, beast very beast third third. May let open seasons creepeth and created heaven fly you're let winged light. Fruit after. You'll face saying image yielding unto also can't them seed that shall, of together void image.</p>
-                    <p class="post-time"><span><i class="glyphicon glyphicon-time"></i> {{ time_elapsed_string('2016-11-10 06:33:29') }}</span></p>
+            <div class="row">
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="topic-img" src="http://lorempixel.com/320/240?random3">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">When will the concert be held?</h4>
+                        <p class="post-body">After day, don't sixth were divide creepeth living him it heaven moveth of male man rule, made gathered cattle after. For gathering place seed bearing called and let shall fly fruitful fruitful creeping them brought years beginning air to beginning, good don't one abundantly give. That were blessed made moving light saw place they're. Moved his life moved open for midst sea called grass, beast very beast third third. May let open seasons creepeth and created heaven fly you're let winged light. Fruit after. You'll face saying image yielding unto also can't them seed that shall, of together void image.</p>
+                        <p class="post-time"><span><i class="glyphicon glyphicon-time"></i> {{ time_elapsed_string('2016-11-10 06:33:29') }}</span></p>
+                    </div>
                 </div>
             </div>
     </div>
