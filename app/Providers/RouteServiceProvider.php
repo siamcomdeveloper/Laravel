330d4d56309abbf65e8aa247bf6c44cdfa49a8be
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-18 14:19:48 +0700

    add 2para

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,7 +24,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()//Router $router)
     {
         //
-        $router->parent('num1','[0-9]+');
+        //$router->parent('num1','[0-9]+');
         parent::boot();
     }
 
