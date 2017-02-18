0d2015cc818421c50907b60b5ee7f678bbc3a33d
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-18 22:10:20 +0700

    form

--- resources/views/admin/form/blog.blade.php
+++ resources/views/admin/form/blog.blade.php
@@ -5,22 +5,12 @@
     <div class='row'>
         <form>
             <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+              <label for="topic">Topic</label>
+              <input type="text" class="form-control" id="Topic" placeholder="Topic">
             </div>
             <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <div class="form-group">
-              <label for="exampleInputFile">File input</label>
-              <input type="file" id="exampleInputFile">
-              <p class="help-block">Example block-level help text here.</p>
-            </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox"> Check me out
-              </label>
+              <label for="content">Content</label>
+              <textarea class="form-control" id="Content" placeholder="Content">
             </div>
             <button type="submit" class="btn btn-default">Submit</button>
           </form>
