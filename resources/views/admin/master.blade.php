e4b43fa7f8ee2082de91732dc1dd5fc51e6ae5f9
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-18 23:35:16 +0700

    form

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -7,7 +7,7 @@
     main{
         padding: 50px;
     }
-    header{
+    /*header{
         padding : 25px;
         background: red;
         color: #FFF;
@@ -18,23 +18,26 @@
         background: blue;
         color: #FFF;
         text-align: center;
-    }
+    }*/
     section{
         padding: 25px;
+    }
+    .fl{
+        float: left;
     }
 </style>
 </head>
 
 <body>
     <header>
-        Header
+        
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </header>
     <section id="main">
         @yield('content')
     </section>
     <footer>
-        Footer
+        
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </footer>
 </body>
