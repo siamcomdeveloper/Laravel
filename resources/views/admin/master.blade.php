2bf3366e09bd972d0d616233f30cb2e66cfc6f35
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 01:44:06 +0700

    form

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -50,7 +50,7 @@
         padding: 25px;
     }
     
-    .table-striped .blog{
+    .well{
        background-color: red;
     }
 </style>
@@ -65,32 +65,34 @@
         @yield('content')
     </section>
     <footer>
-    <div class="blog">
-        <table class="table blog">
-            <tr>
-                <td>
-                    Pic1
-                </td>
-                <td>
-                    Content1
-                </td>
-                <td>
-                    User1
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Pic2
-                </td>
-                <td>
-                    Content2
-                </td>
-                <td>
-                    User2
-                </td>
-            </tr>
-        </table>
-    </div>
+    <div class="well">
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img class="media-object" src="http://placekitten.com/150/150">
+  		</a>
+  		<div class="media-body">
+                    <h4 class="media-heading">Receta 1</h4>
+                    <p class="text-right">By Francisco</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+                    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+                    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+                    Aliquam in felis sit amet augue.</p>
+                    <ul class="list-inline list-unstyled">
+                        <li><span><i class="glyphicon glyphicon-calendar"></i> 2 days, 8 hours </span></li>
+                        <li>|</li>
+                        <span><i class="glyphicon glyphicon-comment"></i> 2 comments</span>
+                        <li>|</li>
+                        <li>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star-empty"></span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <nav aria-label="Page navigation" class="text-center">
             <ul class="pagination">
                <li><a href="#">Previous</a></li>
