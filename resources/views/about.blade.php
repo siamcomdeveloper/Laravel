0857d744e120887e097ef7bb5c6f7a9d62bb2a8d
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-18 14:42:44 +0700

    add 2para

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,13 @@
 Hello : {{ $name. ' '. $last_name }}
 Hello : {{ '<p>' .  $name. ' '. $last_name . '</p>' }}
 Hello : {!! '<p>' .  $name. ' '. $last_name . '</p>' !!}
+
+@if($name=='siam')
+    yes
+@else
+    no
+@endif
+
 <?php
     if($name=='siam'){
         echo 'true';
