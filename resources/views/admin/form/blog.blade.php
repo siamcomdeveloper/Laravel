cfd0a7e88e14e18937e3396684cd4df18eba2b7f
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-18 22:12:50 +0700

    form

--- resources/views/admin/form/blog.blade.php
+++ resources/views/admin/form/blog.blade.php
@@ -6,11 +6,11 @@
         <form>
             <div class="form-group">
               <label for="topic">Topic</label>
-              <input type="text" class="form-control" id="Topic" placeholder="Topic">
+              <input type="text" class="form-control" placeholder="Topic">
             </div>
             <div class="form-group">
               <label for="content">Content</label>
-              <textarea class="form-control" id="Content" placeholder="Content">
+              <textarea class="form-control" rows="8" cols="40"></textarea>
             </div>
             <button type="submit" class="btn btn-default">Submit</button>
           </form>
