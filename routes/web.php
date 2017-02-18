99de71e762a8b9a66d825731359803afd3dd02aa
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-18 14:12:48 +0700

    add 2para

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ Route::get('/about', function () {
 
 //Route parameter type 1 (require parameter)
 Route::get('/plus/{num1?}/{num2?}', function ($num1=0,$num2=0) {
-    echo $num1 . '+' . $num2 . '=' . ($num1+$num2);
-})->where('num1','[0-9]+');
+    //echo $num1 . '+' . $num2 . '=' . ($num1+$num2);
+    echo $num1 . '&' . $num2;
+})->where('num1','[0-9]+')->where('num2','[a-z]+');
 //regular Expression
