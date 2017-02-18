4d8b0b5580107c6f6aefe3e817bce13735012e0b
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-18 16:07:16 +0700

    add 2para

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -27,6 +27,7 @@
         Header
     </header>
     <section id="main">
+        INDEX
         Test Text Section Main Test Text Section Main
         Test Text Section Main Test Text Section Main
         Test Text Section Main Test Text Section Main
