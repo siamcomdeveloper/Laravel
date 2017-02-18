68fd3a4d77d3a8fd5449b91ff9fea45d22da3a2e
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-18 22:01:48 +0700

    form

--- resources/views/admin/list/blog.blade.php
+++ resources/views/admin/list/blog.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
+        <a class="btn btn-primary" href="{{url('admin/blog/create')}}">Create</a>
         <table class="table table-striped">
             <tr>
                 <th>
