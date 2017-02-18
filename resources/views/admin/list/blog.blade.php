2c82c52614adffc1e54dc18be8f9245dfca5c2ea
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-18 21:53:25 +0700

    first controller

--- resources/views/admin/list/blog.blade.php
+++ resources/views/admin/list/blog.blade.php
@@ -1,5 +1,23 @@
 @extends('admin.master')
 
 @section('content')
-
+<div class="container">
+    <div class="row">
+        <table class="table">
+            @foreach($objs as $obj)
+            <tr>
+                <td>
+                    {{ $obj->id }}
+                </td>
+                <td>
+                    {{ $obj->topic }}
+                </td>
+                <td>
+                    {{ $obj->content }}
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
 @stop
