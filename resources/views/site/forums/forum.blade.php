f9cceeacc9787da6f59adc278324a46c31dcb311
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 17:38:11 +0700

    forums

--- resources/views/site/forums/forum.blade.php
+++ resources/views/site/forums/forum.blade.php
@@ -27,7 +27,7 @@
     </header>
     <section id="main">
         @foreach($objs as $row){
-           {{ $row[0]['id'] }}
+           {{ $row['id'] }}
         }
         <div id="style-well1" class="well">
             <div class="row">
