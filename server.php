248c6628a1049be20d25fa7ab74690c1c5122f4f
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 22:04:32 +0700

    forums

--- server.php
+++ server.php
@@ -1,4 +1,23 @@
 <?php
+$answerTable = array ( 0 => array ( "x" =>  0, "y" =>  1, "direction" => "North"),
+                       1 => array ( "x" =>  1, "y" =>  1, "direction" => "North East"),
+                       2 => array ( "x" =>  1, "y" =>  0, "direction" => "East"),
+                       3 => array ( "x" =>  1, "y" => -1, "direction" => "South East"),
+                       4 => array ( "x" =>  0, "y" => -1, "direction" => "South"),
+                       5 => array ( "x" => -1, "y" => -1, "direction" => "South West"),
+                       6 => array ( "x" => -1, "y" =>  0, "direction" => "West"),
+                       7 => array ( "x" => -1, "y" =>  1, "direction" => "North West")
+                );
+/*
+Conditions;
+L : Step - 2;
+R : Step + 2;
+Wx : Step + x;
+
+equation; find direction & position
+index = Step % 8;
+*/
+
 for ($i=0; $i < 3; $i++) {
         $line = readline("Command: ");
         readline_add_history($line);
