99d3028367614d64cb433828a67a781685353852
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 15:48:30 +0700

    forums

--- resources/views/site/forums/forum.blade.php
+++ resources/views/site/forums/forum.blade.php
@@ -56,7 +56,7 @@
                         <p class="post-body">After day, don't sixth were divide creepeth living him it heaven moveth of male man rule, made gathered cattle after. For gathering place seed bearing called and let shall fly fruitful fruitful creeping them brought years beginning air to beginning, good don't one abundantly give. That were blessed made moving light saw place they're. Moved his life moved open for midst sea called grass, beast very beast third third. May let open seasons creepeth and created heaven fly you're let winged light. Fruit after. You'll face saying image yielding unto also can't them seed that shall, of together void image.</p>
                         <p class="post-time"><span><i class="glyphicon glyphicon-time"></i> {{ time_elapsed_string('2016-11-10 06:33:29') }}</span></p>
                     </div>
-                    <div class="column3 col-lg-2">
+                    <div class="column3 col-lg-2" sytle="height: 100%; display: table-row;">
                         <div class="row" sytle="height: 100%; display: table-row;">
                             <img class="avatar-img img-circle" src="https://api.adorable.io/avatars/250/zlatan">
                             <h4 class="name">Zlatan Ibrahimović</h4>
