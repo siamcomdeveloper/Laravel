45024f03a688bb07acc231b827ce96abc36f5673
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 17:44:00 +0700

    forums

--- resources/views/site/forums/forum.blade.php
+++ resources/views/site/forums/forum.blade.php
@@ -29,7 +29,7 @@
         <!--@foreach($objs as $row){
            {{ $row['id'] }}
         }-->
-        {{ var_dump($objs) }}
+        
         <div id="style-well1" class="well">
             <div class="row">
                 <div class="column1 col-lg-2">
