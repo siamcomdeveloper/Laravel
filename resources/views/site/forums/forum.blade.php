fec0fa626855862244b805af99e515d6e515cc94
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 17:44:45 +0700

    forums

--- resources/views/site/forums/forum.blade.php
+++ resources/views/site/forums/forum.blade.php
@@ -26,8 +26,7 @@
         <p class="forumsPosts">Posts</p>
     </header>
     <section id="main">
-        
-        
+        {{ var_dump($objs) }}
         <div id="style-well1" class="well">
             <div class="row">
                 <div class="column1 col-lg-2">
