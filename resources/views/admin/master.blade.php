7251822a408acb9128e86bb60f4e251d283ac166
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 14:56:13 +0700

    form

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -142,8 +142,11 @@
 
 <body>
     <header>
-        
+        <link rel="stylesheet" href="{{ asset('css/forums.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <!--
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        -->
     </header>
     <section id="main">
         @yield('content')
@@ -200,8 +203,11 @@
                <li><a href="#">Next</a></li>
             </ul>
         </nav>
+        <link rel="stylesheet" href="{{ asset('bootstrap/js/bootstrap.min.js') }}">
+        <!--
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </footer>
+        -->
+  </footer>
 </body>
 
 </html>
