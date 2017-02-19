886cd5fc3a290e491e05cda3ea68c3db8b09fb9c
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 20:34:59 +0700

    forums

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::get('/forums/{page?}', function ($page=1) {
     $display_page = 8;
     //calculate remain post of last page
     $pluspage = $total%$display_page;
-    if ($pluspage > 0) pluspage = 1;
+    if ($pluspage > 0) $pluspage = 1;
     else $pluspage = 0;
     $allpage = floor($total/$display_page) + $pluspage;
     /////////////////////////////////////
