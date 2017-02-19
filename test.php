81c6de27ea8a29db35a5e4f1c4d20f76713b4a81
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 22:30:54 +0700

    forums

--- test.php
+++ test.php
@@ -1,4 +1,38 @@
 <?php
+$input = $argv[1];
+
+/*
+Conditions;
+L : Step - 2;
+R : Step + 2;
+WN : Step + N;
+
+equation; find direction & position
+index anserTable = Step % 8;
+*/
+
+$Step = 0;
+for($i = 0; $i < sizeof($input); $i++){
+    if ($input[$i] == "R") {
+        $Step += 2;
+    }
+    else if($input[$i] == "L"){
+        $Step -= 2;
+    }
+    else if($input[$i] == "W"){
+        $num = "";
+        for($j = $i+1; $j < sizeof($input); $j++)
+            if (preg_match('/[0-9]+$/', $input[$j]))
+                $num .= $input[$j];
+        $Step += $num;
+    }
+    else {
+        return "input invalid" . "\r\n";
+    }
+}
+
+echo $Step . "\r\n";
+
 $answerTable = array ( 0 => array ( "x" =>  0, "y" =>  1, "direction" => "North"),
                        1 => array ( "x" =>  1, "y" =>  1, "direction" => "North East"),
                        2 => array ( "x" =>  1, "y" =>  0, "direction" => "East"),
@@ -8,26 +42,7 @@ $answerTable = array ( 0 => array ( "x" =>  0, "y" =>  1, "direction" => "North"
                        6 => array ( "x" => -1, "y" =>  0, "direction" => "West"),
                        7 => array ( "x" => -1, "y" =>  1, "direction" => "North West")
                 );
-/*
-Conditions;
-L : Step - 2;
-R : Step + 2;
-Wx : Step + x;
-
-equation; find direction & position
-index = Step % 8;
-*/
-echo $argv[1] . "\r\n";
-
-/*
-//preg_match($pattern, $subject)
-if (preg_match('/[0-9]+$/', $line)) {
-  // contains only 0-9
-} else {
-  // contains other stuff
-}
-*/
-echo $answerTable[0]["x"] . "\r\n";
-echo $answerTable[0]["y"] . "\r\n";
-echo $answerTable[7]["direction"] . "\r\n";
+//echo $answerTable[0]["x"] . "\r\n";
+//echo $answerTable[0]["y"] . "\r\n";
+//echo $answerTable[7]["direction"] . "\r\n";
 ?>
