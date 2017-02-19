4adf14c972bead428d140cb1d608d8784d03fbb5
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-19 14:35:03 +0700

    form

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -142,8 +142,7 @@
 
 <body>
     <header>
-        
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="./css/bootstrap.css" rel="stylesheet">
     </header>
     <section id="main">
         @yield('content')
